1b55fed60ce227723aa03b63bb21007e9514d6fc
Rick Farina (Zero_Chaos) <zerochaos@gentoo.org>
2021-07-29 22:06:38 -0400

    improve diff_wordlist to filter things too long and too short

--- wordlist/diff_wordlist.sh
+++ wordlist/diff_wordlist.sh
@@ -1,13 +1,39 @@
-#!/bin/sh
+#!/bin/bash
 before="$(wc -l "${1}" | awk '{print $1}')"
 filtered_wordlist="${1/.*}-filtered.words"
 cp "${1}" /dev/shm/"${filtered_wordlist}"
+
+printf "Removing too short words...\n"
+orig="$(wc -l "/dev/shm/${filtered_wordlist}" | awk '{print $1}')"
+awk 'length > 8' "/dev/shm/${filtered_wordlist}" > "/dev/shm/${filtered_wordlist}-temp"
+mv "/dev/shm/${filtered_wordlist}-temp" "/dev/shm/${filtered_wordlist}"
+new="$(wc -l "/dev/shm/${filtered_wordlist}" | awk '{print $1}')"
+minus=$((orig-new))
+printf "removed ${minus} too short words, words remaining: ${new}\n"
+if [ "${new}" = "0" ]; then
+  printf "No words remaining, broken\n"
+  exit 1
+fi
+
+printf "Removing too long words...\n"
+orig="$(wc -l "/dev/shm/${filtered_wordlist}" | awk '{print $1}')"
+awk 'length < 64' "/dev/shm/${filtered_wordlist}" > "/dev/shm/${filtered_wordlist}-temp"
+mv "/dev/shm/${filtered_wordlist}-temp" "/dev/shm/${filtered_wordlist}"
+new="$(wc -l "/dev/shm/${filtered_wordlist}" | awk '{print $1}')"
+minus=$((orig-new))
+printf "removed ${minus} too long words, words remaining: ${new}\n"
+if [ "${new}" = "0" ]; then
+  printf "No words remaining, broken\n"
+  exit 1
+fi
+
+printf "Removing common dict words...\n"
 for i in $(find /usr/share/dict/skullsecurity -type f) $(find /usr/share/dict/raft-wordlists -type f)  $(find /usr/share/dict/seclists/Passwords -type f) $(find /usr/share/dict/theHarvester -type f)$(find /usr/share/dict -maxdepth 1 -type f) ; do
   if [ -e "${i}" ] && [ ! -L "${i}" ]; then
-    #if [ "${i/.csv}" != "${i}" ] && [ "${i/.gz}" != "${i}" ]; then
-    #  printf "Skipping ${i}...\n"
-    #  continue
-    #fi
+    if [ "${i/.csv}" != "${i}" ] || [ "${i/.gz}" != "${i}" ] || [ "${i/.tgz}" != "${i}" ]; then
+      printf "Skipping ${i}...\n"
+      continue
+    fi
     [ "${i}" = "/usr/share/dict/seclists/Passwords/Leaked-Databases/rockyou.txt.tar.gz" ] && continue
     [ "${i}" = "/usr/share/dict/seclists/Passwords/Leaked-Databases/rockyou-withcount.txt.tar.gz" ] && continue
     [ "${i}" = "/usr/share/dict/seclists/Payloads/Zip-Bombs/r.tar.gz" ] && continue
